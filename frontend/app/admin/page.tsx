dbd019f86e1759c684712261eee310d5a4727e62
unniznd <au6695@gmail.com>
2024-11-30 15:32:26 +0530

    FEATURE: Added login protection to admin page and landing page

--- frontend/app/admin/page.tsx
+++ frontend/app/admin/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
 import "./styles.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation"; // For navigation
 import SideBar from "./components/SideBar";
 import BottomNav from "./components/BottomNav";
 import Users from "./components/Users";
 import Roles from "./components/Roles";
 import Logs from "./components/Logs";
 
-
 const AdminPage = () => {
   const [activeTab, setActiveTab] = useState("users");
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login"); // Redirect to login if token is null
+    }
+  }, [router]);
 
   const renderContent = () => {
     switch (activeTab) {
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,18 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("/login"); // Redirect to login if token is null
+    }
+  }, [router]);
+  
   return (
    <>
    </>
