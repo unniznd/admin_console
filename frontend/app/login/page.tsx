ab9581e94a0de652bb27f683b8c6165e03294d9f
unniznd <au6695@gmail.com>
2024-11-30 12:28:48 +0530

    FEATURE: Login UI Added

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import "./styles.css";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+const LoginPage = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setIsLoading(false);
+  };
+
+  return (
+    <div className="login-container">
+      
+      <form onSubmit={handleSubmit} className="login-form">
+      <h1>Login</h1>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
