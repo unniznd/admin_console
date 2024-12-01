7076d89aadac5cde62dd4c29ee47d57e4a84d364
unniznd <au6695@gmail.com>
2024-12-01 22:02:11 +0530

    FEATURE: Backend API Documentation added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,24 +1,27 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from admin_console.views import CustomAuthToken, get_user_data
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Backend API",
+      default_version='v1',
+      description="Admin Console API Documentation",
+      terms_of_service="http://localhost:8000/redoc/",
+      contact=openapi.Contact(email="au6695@gmail.com"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
     path('admin/', admin.site.urls),
     path('api/admin/', include('admin_console.urls')),
     path('api/login/', CustomAuthToken.as_view(), name='login'),
